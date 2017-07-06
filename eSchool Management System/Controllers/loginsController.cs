52f6b94228c8ad102fef7294c686d799b4d7b166
onchari wycliffe <onchariwycliffe@gmail.com>
2017-07-06 18:36:57 +0300

    Add project files.

---
+++ eSchool Management System/Controllers/loginsController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using eSchool_Management_System.Models;
+
+namespace eSchool_Management_System.Controllers
+{
+    public class loginsController : Controller
+    {
+       // private merry_cliff_academyEntities db = new merry_cliff_academyEntities();
+        // GET: logins
+        public ActionResult Index()
+        {
+
+            return View();
+        }
+
+        //POST : Home/Index
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Index(Models.login log )
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    using (merry_cliff_academyEntities db = new merry_cliff_academyEntities())
+                    {
+                        var obj = db.logins.FirstOrDefault(a => a.username.Equals(log.username) && a.password.Equals(log.password));
+                        if (obj != null)
+                        {
+                            Session["UserId"] = obj.loginId;
+                            Session["username"] = obj.username;
+                            return RedirectToAction("Index", "Home");
+                        }
+
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e);
+                    throw;
+                }
+            }
+
+            return View(log);
+        }
+
+
+    }
+}
---
+++ eSchool Management System/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("eSchool_Management_System")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("eSchool_Management_System")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("7be79427-0eb8-43d7-8b69-3c350c9ef4f4")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
